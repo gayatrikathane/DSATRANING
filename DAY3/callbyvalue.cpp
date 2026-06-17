26e75f5cb691bc24b6638a5f3f4c6786c3c2bbbe
Gaytri <gayatrikathane17@gmail.com>
2026-06-17 16:29:23 +0530

    DAY11

--- DAY3/callbyvalue.cpp
+++ DAY3/callbyvalue.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int change(int x)
 {
     x = 100;
-    cout<<"Value of a inside function: "<<x<<endl;
+    cout << "Value of a inside function: " << x << endl;
 }
 int main()
 {
     int a = 10;
     change(a);
-    cout<<"Value of a inside main: "<<a;
+    cout << "Value of a inside main: " << a;
     return 0;
 }
