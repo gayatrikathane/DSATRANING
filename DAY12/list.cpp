d3bf95743d899944fdc9d4b1e9c789977d409485
Gaytri <gayatrikathane17@gmail.com>
2026-06-21 12:48:43 +0530

    DAY1

--- DAY12/list.cpp
+++ DAY12/list.cpp
@@ -1,20 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
 
+    list<int> ls;
 
-using namespace std ;
-int main(){
+    ls.push_back(10);
+    ls.emplace_back(20);
+    ls.push_back(30);
+    ls.emplace_back(40);
+    ls.emplace_back(50);
+    ls.emplace_back(60);
+    ls.emplace_back(60);
+    ls.emplace_back(40);
+    ls.emplace_back(40);
+    ls.emplace_back(100);
+    ls.emplace_back(100);
+    ls.emplace_back(60);
     
-    list<int>l 
-       
 
+    ls.remove(100);
+    for (auto it = ls.begin(); it != ls.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+    for (auto it : ls)
+    {
+        cout << it << " ";
+    }
+    for (auto it : ls)
+    {
+        cout << it << " ";
+    }
 
-
-
-
-
-
-
-
-
-    return 0 ; 
+    return 0;
 }
