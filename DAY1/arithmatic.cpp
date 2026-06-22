3757433118f14f5733e6506b153f90060831f196
Gaytri <gaytrikathane40@gmail.com>
2026-06-22 10:30:15 +0530

    DAY1

--- DAY1/arithmatic.cpp
+++ DAY1/arithmatic.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int main()
 {
-    int a = 10, b = 5;
-    cout << (a + b) << endl; //`15
-    cout << (a - b) << endl; //5
-    cout << (a * b) << endl; //50
-    cout << (a / b) << endl; //2
-    cout << (a % b) << endl; //0
+    int a = 5, b = 10;
+    cout << a + b << endl; // 15
+    cout << a - b << endl; //-5
+    cout << a * b << endl; // 50
+    cout << a / b << endl;
     return 0;
 }
